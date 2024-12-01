3eaf3bdeb298451c0368ef17c5bd215889736ba4
Jonathan Bruce <me@jonathanbruce.ca>
2024-12-01 10:56:22 -0500

    Question 2

--- D01/src/main.rs
+++ D01/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn get_input() -> Vec<Vec<u64>> {
     let contents = fs::read_to_string("./D01/input.txt")
@@ -20,11 +21,22 @@ fn main() {
     right.sort();
 
     let mut sum: u64 = 0;
+    let mut similarity: u64 = 0;
+    let mut grouped_right: HashMap<u64, u64> = HashMap::new(); 
 
     for i in 0..left.len()
     {
         sum += left[i].abs_diff(right[i]);
+
+        *grouped_right.entry(right[i]).or_insert(0) += 1;
+    }
+
+    for i in 0..left.len()
+    {
+        let default:u64 = 0;
+        similarity += left[i] * grouped_right.get(&left[i]).or(Option::from(&default)).expect("Key not missing.");
     }
 
    println!("Sum of differences: {sum}");
+   println!("Similarity: {similarity}");
 }
