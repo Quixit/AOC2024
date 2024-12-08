5ee4cc023988139136976a4e4d2208525e40548f
Jonathan Bruce <me@jonathanbruce.ca>
2024-12-08 11:55:10 -0500

    Day 6

---
+++ D07/src/main.rs
@@ -0,0 +1,88 @@
+use std::fs;
+
+fn get_input() -> Vec<Vec<u64>> {
+
+    let contents: Vec<String> = fs::read_to_string("./D07/input.txt")
+        .expect("File exists.").replace("\r\n", "\n")
+        .split("\n")
+        .map(|x| x.to_string())
+        .collect();
+
+    let result: Vec<Vec<u64>> =  contents.iter()
+        .map(|l| l.replace(":", "")
+            .split(' ')
+            .map(|v| v.parse::<u64>()
+            .unwrap())
+            .collect()
+        )
+        .collect();
+
+    return  result;
+}
+
+
+fn main() {
+    let input = get_input();      
+    let mut sum: u64 = 0; 
+    let mut sum_two: u64 = 0;
+
+    for equation in input {
+        let one = evaluate_equation(equation[1..equation.len()].to_vec()).contains(&equation[0]);
+        let two = evaluate_equation_two(equation[1..equation.len()].to_vec()).contains(&equation[0]);
+
+        if one  {
+            sum += equation[0];
+        }
+
+        if two {
+            sum_two += equation[0];
+        }
+    }
+
+    println!("Calibration Result: {sum}");
+    println!("Calibration Result Two: {sum_two}");
+}
+
+fn evaluate_equation(equation: Vec<u64>) -> Vec<u64> {
+    if equation.len() == 1 {
+        return [equation[0]].to_vec();
+    }   
+
+    let mut equation_one = [equation[0] + equation[1]].to_vec();   
+    let mut rest =  equation.clone()[2..equation.len()].to_vec();
+
+    equation_one.append(&mut rest);
+
+    let mut equation_two = [equation[0] * equation[1]].to_vec();   
+    let mut rest =  equation.clone()[2..equation.len()].to_vec();
+
+    equation_two.append(&mut rest);
+
+    let mut result = evaluate_equation(equation_one);
+    let mut result_two = evaluate_equation(equation_two);
+    result.append(&mut result_two);
+
+    return result
+}
+
+fn evaluate_equation_two(equation: Vec<u64>) -> Vec<u64> {
+    if equation.len() == 1 {
+        return [equation[0]].to_vec();
+    }   
+
+    let mut equation_one = [equation[0] + equation[1]].to_vec();   
+    let mut rest =  equation.clone()[2..equation.len()].to_vec();
+
+    equation_one.append(&mut rest);
+
+    let mut equation_two = [equation[0] * equation[1]].to_vec();   
+    let mut rest =  equation.clone()[2..equation.len()].to_vec();
+
+    equation_two.append(&mut rest);
+
+    let mut result = evaluate_equation(equation_one);
+    let mut result_two = evaluate_equation(equation_two);
+    result.append(&mut result_two);
+
+    return result
+}
